e39b894bd8f382d756cbf3fbf5de0b0486cdd53a
LEGION <rc.vasquez27@gmaill.com>
2025-06-26 15:17:40 -0600

    fix en config y plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,10 @@
-const { defineConfig } = require('cypress');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
     },
-    baseUrl: 'https://practicetestautomation.com'
-  }
-});
+  },
+};
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,3 +16,5 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import '@shelex/cypress-allure-plugin';
+
+
