da82021e9c1f13619f4bddcc15b7f2d851f40969
LEGION <rc.vasquez27@gmaill.com>
2025-06-26 11:39:22 -0600

    integracion de fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,15 @@
 import LoginPage from '../pages/LoginPage';
 
-describe('Login con Page Object Model', () => {
+describe('Login con Page Object Model', function() {
   const loginPage = new LoginPage();
 
   beforeEach(() => {
+    cy.fixture('loginData').as('data');
     loginPage.visit();
   });
 
-  it('Login exitoso con credenciales válidas', () => {
-    loginPage.loginUser('student', 'Password123');
+  it('Login exitoso con credenciales válidas', function() {
+    loginPage.loginUser(this.data.validUser.username, this.data.validUser.password);
     loginPage.assertLoginSuccess();
   });
 });
@@ -17,17 +18,18 @@ describe('Login tests negativos', () => {
   const loginPage = new LoginPage()
 
   beforeEach(() => {
+    cy.fixture('loginData').as('data');
     cy.visit('/practice-test-login/')
   })
 
-  it('Debería mostrar error con usuario incorrecto', () => {
-    loginPage.loginUser('usuario_incorrecto', 'password_valido')
+  it('Debería mostrar error con usuario incorrecto', function() {
+    loginPage.loginUser(this.data.invalidUser.username, this.data.invalidUser.password)
     loginPage.assertLoginFailure('Your username is invalid!')
   })
 
-  it('Debería mostrar error con contraseña incorrecta', () => {
-    loginPage.loginUser('tomsmith', 'password_incorrecto')
-    loginPage.assertLoginFailure('Your username is invalid!')
+  it('Debería mostrar error con contraseña incorrecta', function() {
+    loginPage.loginUser(this.data.invalidPassword.username, this.data.invalidPassword.password)
+    loginPage.assertLoginFailure('Your password is invalid!')
   })
 
 })
