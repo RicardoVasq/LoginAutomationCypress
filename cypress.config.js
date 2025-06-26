207531068fb987bce148e76e0dcc5b725148adb4
LEGION <rc.vasquez27@gmaill.com>
2025-06-26 12:34:39 -0600

    integracion con allure y Github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://practicetestautomation.com',
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    supportFile: 'cypress/support/e2e.js'
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+    baseUrl: 'https://practicetestautomation.com'
   }
 });
