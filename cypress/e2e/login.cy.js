386340aefcdef580c8b37a5da750ca3d32bd11d8
LEGION <rc.vasquez27@gmaill.com>
2025-06-26 15:43:39 -0600

    Fix en test negativos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -19,7 +19,7 @@ describe('Login tests negativos', () => {
 
   beforeEach(() => {
     cy.fixture('loginData').as('data');
-    cy.visit('/practice-test-login/')
+    cy.visit('/practice-test-login/');
   })
 
   it('Debería mostrar error con usuario incorrecto', function() {
